d130e7b9976f1c3110c43ec959cc35de518303d3
Joshua Groves <joshua.groves@3esi-enersight.com>
2017-08-09 21:01:30 -0600

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace ClassLibrary1
+{
+    public class A<T> {
+        public T B { get; set; }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.TypeScript;
+using NSwag.CodeGeneration.TypeScript;
+using NSwag.SwaggerGeneration;
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = GenerateTypeScript(typeof(A<>).Assembly.Location, new[] { typeof(A<>) }).GetAwaiter().GetResult();
+        }
+
+        public static async Task<string> GenerateTypeScript(string assemblyLocation, IEnumerable<Type> models)
+        {
+            var clientSettings = new SwaggerToTypeScriptClientGeneratorSettings
+            {
+                Template = TypeScriptTemplate.Fetch,
+                GenerateDtoTypes = true,
+                GenerateClientClasses = false,
+                GenerateClientInterfaces = false,
+                TypeScriptGeneratorSettings =
+                {
+                    TypeStyle = TypeScriptTypeStyle.Interface,
+                    NullHandling = NullHandling.JsonSchema,
+                    DateTimeType = TypeScriptDateTimeType.MomentJS
+                }
+            };
+            var swaggerGenerator = new AssemblyTypeToSwaggerGenerator(new AssemblyTypeToSwaggerGeneratorSettings
+            {
+                AssemblyPath = assemblyLocation,
+                DefaultPropertyNameHandling = PropertyNameHandling.CamelCase
+            });
+            var document = await swaggerGenerator.GenerateAsync(models.Select(m => m.FullName).ToArray());
+            return new SwaggerToTypeScriptClientGenerator(document, clientSettings, new TypeScriptTypeResolver(clientSettings.TypeScriptGeneratorSettings, document)).GenerateFile();
+        }
+    }
+}
